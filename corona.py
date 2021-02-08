93085438f82c10fa1cd2af324ebbab8ecf54d2e4
thetajwar2003 <rahmant7@bxscience.edu>
2021-02-08 18:14:21 -0500

    initial commit

---
+++ corona.py
@@ -0,0 +1,180 @@
+import requests
+import json
+import pyttsx3
+import speech_recognition as sr
+import re
+import threading
+import time
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+PROJECT_TOKEN = os.getenv('PROJECT_TOKEN')
+RUN_TOKEN = os.getenv('RUN_TOKEN')
+
+
+class Data:
+    def __init__(self, api_key, project_token):
+        self.api_key = api_key
+        self.project_token = project_token
+        self.params = {"api_key": self.api_key}
+        self.data = self.get_data()
+
+    def get_data(self):
+        response = requests.get(f'https://www.parsehub.com/api/v2/projects/{self.project_token}/last_ready_run/data',
+                                params=self.params)
+        data = json.loads(response.text)
+        return data
+
+    def get_total_cases(self):
+        data = self.data['total']
+        for content in data:
+            if content['name'] == "Coronavirus Cases:":
+                return content['num_cases']
+
+    def get_total_deaths(self):
+        data = self.data['total']
+        for content in data:
+            if content['name'] == "Deaths:":
+                return content['num_cases']
+
+    def get_total_recovered(self):
+        data = self.data['total']
+        for content in data:
+            if content['name'] == "Recovered:":
+                return content['num_cases']
+        return "0"
+
+    def get_country_data(self, country):
+        data = self.data['countries']
+        for content in data:
+            if content['name'].lower() == country.lower():
+                return content
+        return "0"
+
+    def get_states_data(self, state):
+        data = self.data['state_name']
+        for content in data:
+            if content['name'].lower() == state.lower():
+                return content
+        return "0"
+
+    def get_list_of_countries(self):
+        countries = []
+        for country in self.data['countries']:
+            countries.append(country['name'].lower())
+        return countries
+
+    def get_list_of_states(self):
+        states = []
+        for state in self.data['state_name']:
+            states.append(state['name'].lower())
+        return states
+
+    def update_data(self):
+        response = requests.post(f'https://www.parsehub.com/api/v2/projects/{self.project_token}/run', params=self.params)
+
+        def poll():
+            time.sleep(0.1)
+            old_data = self.data
+            while True:
+                new_data = self.get_data()
+                if new_data != old_data:
+                    self.data = new_data
+                    print("Data updated")
+                    break
+                time.sleep(5)
+
+
+        t = threading.Thread(target=poll)
+        t.start()
+
+
+def speak(text):
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+
+
+def get_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        said = ""
+        try:
+            said = r.recognize_google(audio)
+        except Exception as e:
+            print("Exception:", str(e))
+    return said.lower()
+
+
+def main():
+    print("Started Corona")
+    data = Data(API_KEY, PROJECT_TOKEN)
+    END_PHRASE = "stop"
+    country_list = list(data.get_list_of_countries())
+    state_list = list(data.get_list_of_states())
+
+    TOTAL_PATTERNS = {
+                    re.compile("[\w\s]+ total [\w\s]+ cases"): data.get_total_cases,
+                    re.compile("[\w\s]+ total cases"): data.get_total_cases,
+                    re.compile("[\w\s]+ total [\w\s]+ deaths"): data.get_total_deaths,
+                    re.compile("[\w\s]+ total deaths"): data.get_total_deaths(),
+                    re.compile("[\w\s]+ total [\w\s]+ recovered"): data.get_total_recovered,
+                    re.compile("[\w\s]+ total recovered"): data.get_total_recovered
+                    }
+
+    COUNTRY_PATTERNS = {
+                        re.compile("[\w\s]+ cases [\w\s]+"): lambda country: data.get_country_data(country)['total_cases'],
+                        re.compile("[\w\s]+ deaths [\w\s]+"):lambda country: data.get_country_data(country)['total_deaths'],
+                        re.compile("[\w\s]+ recovered [\w\s]+"): lambda country: data.get_country_data(country)['total_recovered'],
+                        }
+
+    STATE_PATTERNS = {
+                        re.compile("[\w\s]+ cases [\w\s]+"): lambda state: data.get_states_data(state)['state_cases'],
+                        re.compile("[\w\s]+ deaths [\w\s]+"): lambda state: data.get_states_data(state)['state_death'],
+                        re.compile("[\w\s]+ tests [\w\s]+"): lambda state: data.get_states_data(state)['state_testings'],
+                        }
+    UPDATE_COMMAND = "update"
+    while True:
+        print("Listening...")
+        text = get_audio()
+        print(text)
+        result = None
+
+        for pattern, func in STATE_PATTERNS.items():
+            if pattern.match(text):
+                for state in state_list:
+                    if state in text:
+                        result = func(state)
+                        break
+
+        for pattern, func in COUNTRY_PATTERNS.items():
+            if pattern.match(text):
+                for country in country_list:
+                    if country in text:
+                        result = func(country)
+                        break
+
+        for pattern, func in TOTAL_PATTERNS.items():
+            if pattern.match(text):
+                result = func()
+                break
+
+        if text == UPDATE_COMMAND:
+            result = "Data is being updated. This may take a moment."
+            data.update_data()
+
+        if result:
+            speak(result)
+
+        if text.find(END_PHRASE) != -1:  # stop loop
+            print("Exit")
+            break
+
+main()
+# data = Data(API_KEY, PROJECT_TOKEN)
+# data = data.get_states_data('new york')
+# print(data)
